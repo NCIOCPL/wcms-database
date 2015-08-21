e37d38fd98afc7c30804b91d566088b5d9644e2a
Blair Learn <blairlearn@users.noreply.github.com>
2015-08-21 21:03:31 +0000

    Flesh out search suggest.  Return maximum number of matches.  Add comments.

--- CDRGKCommon/StoredProcedure/usp_SearchSuggestDictionary.sql
+++ CDRGKCommon/StoredProcedure/usp_SearchSuggestDictionary.sql
@@ -6,52 +6,66 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
--- Retrieves a specific term
+-- Retursn a table containing term names or aliases which match the
+-- specified search criteria
 
 CREATE PROCEDURE [dbo].[usp_SearchSuggestDictionary]
-	@searchText nvarchar(2000),
-	@searchType nvarchar(11),
-	@Dictionary nvarchar(10),
-	@Language nvarchar(20),
-	@Audience nvarchar(25),
-	@ApiVers nvarchar(10), -- What version of the API (there may be multiple).
-	@maxResults int = 10
+	@searchText nvarchar(1000), -- text to match
+	@searchType nvarchar(11), -- 'begins', 'contains' or 'magic' (begins followed by contains)
+	@Dictionary nvarchar(10), -- The dictionary to search (term/drug/genetic)
+	@Language nvarchar(20), -- The language to use for search and results
+	@Audience nvarchar(25), -- Target audience.
+	@ApiVers nvarchar(10), -- What version of the API? (There may be multiple.)
+	@maxResults int = 10, -- The maximum number of results to return
+
+	@matchCount int out -- Returns the total number of rows matching the criteria.
 
 AS
 BEGIN
 
+-- Temporary table to hold the search results.
 create table #resultSet(
 	searchType nvarchar(10),
 	TermID int,
 	TermName nvarchar(1000)
 );
 
-declare @beginsTerm nvarchar(100),
-		@containsTerm nvarchar(100)
+-- Separate comparison values for begins vs contains. Doing this here vs. the
+-- middle tier allows us to do the "magic" search with the same set of queries..
+declare @beginsTerm nvarchar(1000),
+		@containsTerm nvarchar(1000)
 
 select @beginsTerm = @searchText + '%';
 select @containsTerm = '%[- ]' + @searchText + '%';
 
-
+-- Gather up the full set of results.  Don't worry about
+-- sorting or limiting to @maxResults yet because we need to
+-- find out the total number of possible matches too.
+-- Prepending begins vs contains provides a means of ordering them
+-- for the "magic" search type.
 insert #resultSet (searchType, TermID, TermName)
 (
+	-- Match term name beginning with @beginsTerm
 	select 'begins' as searchType, TermID, TermName
 	from Dictionary
 	where TermName like @beginsTerm
 	  and Language = @language
 	  and Dictionary = @dictionary
 	  and (@searchType = 'magic' or @searchType = 'begins')
-union
+  
+  union
+
+	-- Match alternate name beginning with @beginsTerm
 	select 'begins' as searchType, d.TermID, dta.OtherName
 	from Dictionary d join DictionaryTermAlias dta on d.TermID = dta.TermID
 	where dta.OtherName like @beginsTerm
 	  and d.Language = @language
 	  and d.Dictionary = @dictionary
 	  and (@searchType = 'magic' or @searchType = 'begins')
-)
 
-insert #resultSet (searchType, TermID, TermName)
-(
+  union
+
+	-- Match term name containing @containsTerm
 	select 'contains' as searchType, TermID, TermName
 	from Dictionary
 	where TermName like @containsTerm
@@ -59,7 +73,10 @@ insert #resultSet (searchType, TermID, TermName)
 	  and Dictionary = @dictionary
 	  and (@searchType = 'magic' or @searchType = 'contains')
 	  and TermID not in (select TermID from #resultSet)
+
 union
+
+	-- Match alternate name containing @containsTerm
 	select 'contains' as searchType, d.TermID, dta.OtherName
 	from Dictionary d join DictionaryTermAlias dta on d.TermID = dta.TermID
 		and dta.Othername not in(select TermName from #resultSet)
@@ -69,16 +86,21 @@ union
 	  and (@searchType = 'magic' or @searchType = 'begins')
 )
 
+-- Get the number of results matching the search criteria.
+select @matchCount = @@RowCount
 
---select COUNT(*) as possible
---from #resultSet
 
+-- Return the requested search results.
 select top (@maxResults) row, TermID, TermName --, Dictionary, Language, Audience, ApiVers, object
 from
 (
 	select ROW_NUMBER() over (order by searchType, termName) as row, *
 	from #resultSet
 ) oresult
+
+
+
+
 
 
 	
