6e025ef3f4ecc4db5a1aa0a5e5461d0ea29544ab
shahaj <webcommauto@users.noreply.github.com>
2015-09-24 10:40:40 +0000

    Added relevant comments for the Insert Keywords sql statement

--- gatekeeper/InsertXMLQuery-SummaryKeyWords.sql
+++ gatekeeper/InsertXMLQuery-SummaryKeyWords.sql
@@ -1,3 +1,5 @@
+/*This insert statement is needed for the GateKeeper summary extractor to correctly extract the new SummaryKeyWords from the CDR Data*/
+
 insert into XMLQuery (Name, QueryText, Comments, UpdateDate, UpdateUserID)
 	  values ('SummaryKeyWords', './SummaryMetaData/SummaryKeyWords', null, GETDATE(),'dbo')
 	  
