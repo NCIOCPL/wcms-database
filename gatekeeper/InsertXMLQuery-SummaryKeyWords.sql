52c7e299890957cdc6d4e8787cdda15f9e724af2
shahaj <webcommauto@users.noreply.github.com>
2015-10-21 18:45:26 +0000

    OCEProject 3696 - update the query to make sure we extract one keyword at a time

--- gatekeeper/InsertXMLQuery-SummaryKeyWords.sql
+++ gatekeeper/InsertXMLQuery-SummaryKeyWords.sql
@@ -1,5 +1,5 @@
 /*This insert statement is needed for the GateKeeper summary extractor to correctly extract the new SummaryKeyWords from the CDR Data*/
 
 insert into XMLQuery (Name, QueryText, Comments, UpdateDate, UpdateUserID)
-	  values ('SummaryKeyWords', './SummaryMetaData/SummaryKeyWords', null, GETDATE(),'dbo')
+	  values ('SummaryKeyWord', './SummaryMetaData/SummaryKeyWords/SummaryKeyWord', null, GETDATE(),'dbo')
 	  
