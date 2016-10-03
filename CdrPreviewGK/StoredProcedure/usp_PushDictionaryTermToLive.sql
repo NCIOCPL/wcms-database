a70a9ee955b86f1f0df32d025c2f5aa64d6221d6
Min Gao <zhuomingao@users.noreply.github.com>
2016-10-03 18:40:16 +0000

    index optimization related change

--- CdrPreviewGK/StoredProcedure/usp_PushDictionaryTermToLive.sql
+++ CdrPreviewGK/StoredProcedure/usp_PushDictionaryTermToLive.sql
@@ -1,102 +1,95 @@
+  
+  
+  
+/* NCI - National Cancer Institute  
+*   
+* Purpose:   
+* - Push Extracted data To CDRPreviewGK   
+*  
+* Objects Used:  
+*  
+*  
+*/  
 
-
-IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[usp_PushDictionaryTermToLive]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
-DROP PROCEDURE [dbo].[usp_PushDictionaryTermToLive]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-/*	NCI - National Cancer Institute
-*	
-*	Purpose:	
-*	- Push Extracted data To CDRPreviewGK	
-*
-*	Objects Used:
-*
-*
-*/
-
-CREATE PROCEDURE dbo.usp_PushDictionaryTermToLive
-	(
-	@TermID		int
-	)
-AS
-BEGIN
-
-	set nocount on 
-	
-	DECLARE @r int
-
-
-	BEGIN
-		EXEC @r =  CDRLiveGK.dbo.usp_ClearDictionaryData @TermID
-		IF (@@ERROR <> 0) or (@r <>0)
-		BEGIN
-			
-			RAISERROR ( 70001, 16, 1, @TermID, 'Dictionary')
-			RETURN 70001
-		END 
-	END
-
-	--**************************************************************************************************************
-
-	INSERT INTO CDRLiveGK.dbo.Dictionary
-		(
-		TermID,
-		TermName,
-		Dictionary,
-		Language,
-		Audience,
-		ApiVers,
-		Object
-		)
-	SELECT 	TermID,
-		TermName,
-		Dictionary,
-		Language,
-		Audience,
-		ApiVers,
-		Object
-	FROM 	CDRPreviewGK.dbo.Dictionary
-	WHERE 	TermID = @TermID
-
-	IF (@@ERROR <> 0)
-	BEGIN
-		
-		RAISERROR ( 70000, 16, 1, @TermID, 'Dictionary')
-		RETURN 70000
-	END 	
-
-	INSERT INTO CDRLiveGK.dbo.DictionaryTermAlias
-		(
-		TermID,
-		Othername,
-		OtherNameType,
-		Language
-		)
-	SELECT	TermID,
-			Othername,
-			OtherNameType,
-			Language
-	FROM 	CDRPreviewGK.dbo.DictionaryTermAlias
-	WHERE 	TermID = @TermID
-
-
-	IF (@@ERROR <> 0)
-	BEGIN
-		
-		RAISERROR ( 70000, 16, 1, @TermID, 'Dictionary')
-		RETURN 70000
-	END 	
-
-END
-
-
-
-GO
-GRANT EXECUTE ON [dbo].[usp_PushDictionaryTermToLive] TO [Gatekeeper_role]
-GO
+if object_id('usp_PushDictionaryTermToLive') is not null
+    drop procedure dbo.usp_PushDictionaryTermToLive  
+go
+CREATE PROCEDURE dbo.usp_PushDictionaryTermToLive  
+ (  
+ @TermID  int  
+ )  
+AS  
+BEGIN  
+  
+ set nocount on   
+   
+ DECLARE @r int  
+  
+  
+ BEGIN  
+  EXEC @r =  CDRLiveGK.dbo.usp_ClearDictionaryData @TermID  
+  IF (@@ERROR <> 0) or (@r <>0)  
+  BEGIN  
+     
+   RAISERROR ( 70001, 16, 1, @TermID, 'Dictionary')  
+   RETURN 70001  
+  END   
+ END  
+  
+ --**************************************************************************************************************  
+  
+ INSERT INTO CDRLiveGK.dbo.Dictionary  
+  (  
+  TermID,  
+  TermName,  
+  Dictionary,  
+  Language,  
+  Audience,  
+  ApiVers,  
+  Object  
+  )  
+ SELECT  TermID,  
+  TermName,  
+  Dictionary,  
+  Language,  
+  Audience,  
+  ApiVers,  
+  Object  
+ FROM  CDRPreviewGK.dbo.Dictionary  
+ WHERE  TermID = @TermID  
+  
+ IF (@@ERROR <> 0)  
+ BEGIN  
+    
+  RAISERROR ( 70000, 16, 1, @TermID, 'Dictionary')  
+  RETURN 70000  
+ END    
+  
+ INSERT INTO CDRLiveGK.dbo.DictionaryTermAlias  
+  (  
+  TermID,  
+  Othername,  
+  OtherNameType,  
+  Language  ,
+  othernamelen
+  )  
+ SELECT TermID,  
+   Othername,  
+   OtherNameType,  
+   Language  ,
+   len(othername)
+ FROM  CDRPreviewGK.dbo.DictionaryTermAlias  
+ WHERE  TermID = @TermID  
+  
+  
+ IF (@@ERROR <> 0)  
+ BEGIN  
+    
+  RAISERROR ( 70000, 16, 1, @TermID, 'Dictionary')  
+  RETURN 70000  
+ END    
+  
+END  
+ GO
+ GRANT EXECUTE ON [dbo].[usp_PushDictionaryTermToLive] TO [Gatekeeper_role]  
+  
